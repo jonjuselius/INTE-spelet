01316bbb496d1e772bab4874af37e112238f4013
Jon Juselius <jon.juselius@gmail.com>
2021-10-12 12:45:13 +0200

    Corrections

--- src/test/java/Equipment/EquipmentTest.java
+++ src/test/java/Equipment/EquipmentTest.java
@@ -10,31 +10,31 @@ import static org.junit.jupiter.api.Assertions.*;
 class EquipmentTest {
 	
 	@Test
-	void EC1NewSwordHasFullCondition() {
+	void newSwordHasFullCondition() {
 		Equipment sword = new Sword();
 		assertEquals(Equipment.MAX_CONDITION, sword.getCondition());
 	}
 	
 	@Test // Upprepning av samma testfall som ovan, fast med Hamcrest
-	void EC1NewSwordHasFullConditionHamcrest() {
+	void newSwordHasFullConditionHamcrest() {
 		Equipment sword = new Sword();
 		assertThat(sword.getCondition(), is(equalTo(Equipment.MAX_CONDITION)));
 	}
 	
 	@Test
-	void EC2NewSwordWithConditionBetweenMinAndMaxSetsConditionInConstructor() {
+	void newSwordWithConditionBetweenMinAndMaxSetsConditionInConstructor() {
 		int condition = (Equipment.MAX_CONDITION + Equipment.MIN_CONDITION) / 2;
 		Equipment sword = new Sword(condition);
 		assertThat(sword.getCondition(), is(equalTo(condition)));
 	}
 	
 	@Test
-	void EC3NewSwordWithConditionUnderMinimumThrowsIAE() {
+	void newSwordWithConditionUnderMinimumThrowsIAE() {
 		assertThrows(IllegalArgumentException.class, () -> new Sword(Equipment.MIN_CONDITION - 1));
 	}
 	
 	@Test
-	void EC4NewSwordWithConditionOverMaximumThrowsIAE() {
+	void newSwordWithConditionOverMaximumThrowsIAE() {
 		assertThrows(IllegalArgumentException.class, () -> new Sword(Equipment.MAX_CONDITION + 1));
 	}
 }
