7b5854a59c33dd82138a557e48c0b6fd1780ec1c
Jon Juselius <jon.juselius@gmail.com>
2021-10-12 11:32:12 +0200

    Hamcrest testing

--- src/test/java/Equipment/EquipmentTest.java
+++ src/test/java/Equipment/EquipmentTest.java
@@ -2,6 +2,9 @@ package Equipment;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EquipmentTest {
@@ -10,5 +13,11 @@ class EquipmentTest {
 	void newSwordHasFullCondition() {
 		Equipment sword = new Sword();
 		assertEquals(Equipment.MAX_CONDITION, sword.getCondition());
+	}
+	
+	@Test
+	void newSwordHasFullConditionHamcrest() {
+		Equipment sword = new Sword();
+		assertThat(sword.getCondition(), is(equalTo(Equipment.MAX_CONDITION)));
 	}
 }
