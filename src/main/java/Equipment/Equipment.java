07f79baaa7fc471aa3f382e29cf452b1051c2f7c
Jon Juselius <jon.juselius@gmail.com>
2021-10-12 11:20:15 +0200

    TDD yellow phase

--- src/main/java/Equipment/Equipment.java
+++ src/main/java/Equipment/Equipment.java
@@ -1,10 +1,11 @@
 package Equipment;
 
 public abstract class Equipment {
+	public static final int MAX_CONDITION = 100;
 	private int condition;
 	
 	public Equipment() {
-		this.condition = 100;
+		this.condition = MAX_CONDITION;
 	}
 	
 	public int getCondition() {
