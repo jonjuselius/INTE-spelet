15acf0177f10739a5d2035fc17853123c2b7aead
Jon Juselius <jon.juselius@gmail.com>
2021-10-13 15:25:55 +0200

    Differentiated between Slots and Items in Inventory

--- src/test/java/Inventory/InventoryTest.java
+++ src/test/java/Inventory/InventoryTest.java
@@ -5,13 +5,19 @@ import org.junit.jupiter.api.Test;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class InventoryTest {
 	
 	@Test
 	void newInventoryHasDefaultCapacity() {
 		Inventory inventory = new Inventory();
-		assertThat(inventory.getItems().size(), is(equalTo(Inventory.DEFAULT_CAPACITY)));
+		assertThat(inventory.getSlots().size(), is(equalTo(Inventory.DEFAULT_CAPACITY)));
 	}
+	
+	@Test
+	void newInventoryHasZeroItems() {
+		Inventory inventory = new Inventory();
+		assertThat(inventory.getItems().size(), is(equalTo(0)));
+	}
+	
 }
