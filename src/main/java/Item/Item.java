9ec1e25910a25922e98d319226529cf79b10dc97
Jon Juselius <jon.juselius@gmail.com>
2021-10-13 14:51:53 +0200

    Added "equipped" attribute and tests, with State Machine Diagram

--- src/main/java/Item/Item.java
+++ src/main/java/Item/Item.java
@@ -1,6 +1,20 @@
-public class Item {
+package Item;
+
+public abstract class Item {
 	
 	private int weight;
 	private int value;
 	
+	public Item(int weight, int value) {
+		this.weight = weight;
+		this.value = value;
+	}
+	
+	public int getWeight() {
+		return weight;
+	}
+	
+	public int getValue() {
+		return value;
+	}
 }
