04e52b7252fcce1885916c420c038fcf6c041242
Jon Juselius <jon.juselius@gmail.com>
2021-10-13 15:05:39 +0200

    Added Inventory and related test case

--- src/main/java/Inventory/Inventory.java
+++ src/main/java/Inventory/Inventory.java
@@ -1,2 +1,18 @@
-package Inventory;public class Inventory {
+package Inventory;
+
+import Item.*;
+import java.util.*;
+
+public class Inventory {
+	
+	public static final int DEFAULT_CAPACITY = 100;
+	private Item[] items;
+	
+	public Inventory() {
+		items = new Item[DEFAULT_CAPACITY];
+	}
+	
+	public List<Item> getItems() {
+		return Collections.unmodifiableList(Arrays.asList(items));
+	}
 }
--- src/test/java/Inventory/InventoryTest.java
+++ src/test/java/Inventory/InventoryTest.java
@@ -1,4 +1,17 @@
+package Inventory;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
 class InventoryTest {
-  
+	
+	@Test
+	void newInventoryHasDefaultCapacity() {
+		Inventory inventory = new Inventory();
+		assertThat(inventory.getItems().size(), is(equalTo(100)));
+	}
 }
