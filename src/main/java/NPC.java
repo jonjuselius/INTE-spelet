bd214b8be55d7835ee5a9988ab17ee6381a6e5d5
lenaludwick <lenaludwick@gmail.com>
2021-10-14 11:18:02 +0200

    Lägger till grov draft av character/player/npc

---
+++ src/main/java/NPC.java
@@ -0,0 +1,10 @@
+public class NPC extends Character {
+
+
+    private int level;
+
+    public NPC(String name, Race race, boolean isAlive, int level) {
+        super(name, race, isAlive);
+        this.level = level;
+    }
+}
