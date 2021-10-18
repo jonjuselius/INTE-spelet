eeb20f09ed6d4217d3c53d7d3603c8a628820a6b
Jasmyn Gunica <jasmyn1651@gmail.com>
2021-10-18 16:00:13 +0200

    Fixar konflikter

--- src/main/java/GameCharacters/NPC.java
+++ src/main/java/GameCharacters/NPC.java
@@ -8,10 +8,13 @@ import Races.*;
 public class NPC extends Character {
 
 
-    private int level;
 
-    public NPC(String name, Race race,Job job, boolean isAlive) {
+    public NPC(String name, Race race,Job job, boolean isAlive, int level) {
         super( name, race, job, isAlive);
+        this.level = level;
+    }
+
+    protected void setLevel(int level) {
         this.level = level;
     }
 }
