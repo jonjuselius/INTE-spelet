15da6f05f6631e945ea2cba2ddcff0b9ddf664ab
Jon Juselius <jon.juselius@gmail.com>
2021-10-19 19:59:21 +0200

    TDD green phase ShieldTest

--- src/test/java/Item/ShieldTest.java
+++ src/test/java/Item/ShieldTest.java
@@ -14,49 +14,70 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ShieldTest {
 	
+	public static final Egg DEFAULT_SHIELD = new Egg();
+	public static final String DEFAULT_NAME = "Mr Default";
+	public static final Race HUMAN = new Human();
+	public static final Race OGRE = new Ogre();
+	public static final Race ELF = new Elf();
+	public static final Job KNIGHT = new Knight();
+	public static final Job MAGICIAN = new Knight();
+	public static final Job HEALER = new Knight();
+	public static final boolean DEFAULT_LIFE_STATUS = true;
+	public static final Character[] DEFAULT_PLAYERS = {
+			new Player(DEFAULT_NAME, HUMAN, KNIGHT, DEFAULT_LIFE_STATUS),
+			new Player(DEFAULT_NAME, OGRE, KNIGHT, DEFAULT_LIFE_STATUS),
+			new Player(DEFAULT_NAME, ELF, KNIGHT, DEFAULT_LIFE_STATUS),
+			new Player(DEFAULT_NAME, HUMAN, MAGICIAN, DEFAULT_LIFE_STATUS),
+			new Player(DEFAULT_NAME, OGRE, MAGICIAN, DEFAULT_LIFE_STATUS),
+			new Player(DEFAULT_NAME, ELF, MAGICIAN, DEFAULT_LIFE_STATUS),
+			new Player(DEFAULT_NAME, HUMAN, HEALER, DEFAULT_LIFE_STATUS),
+			new Player(DEFAULT_NAME, OGRE, HEALER, DEFAULT_LIFE_STATUS),
+			new Player(DEFAULT_NAME, ELF, HEALER, DEFAULT_LIFE_STATUS),
+	};
+	
 	@Test
 	void R28_canUse() {
-		fail();
+		assertTrue(DEFAULT_SHIELD.canBeUsedBy(DEFAULT_PLAYERS[0]));
 	}
 	
 	@Test
 	void R29_canUse() {
-		fail();
+		assertTrue(DEFAULT_SHIELD.canBeUsedBy(DEFAULT_PLAYERS[1]));
 	}
 	
 	@Test
 	void R30_canUse() {
-		fail();
+		assertTrue(DEFAULT_SHIELD.canBeUsedBy(DEFAULT_PLAYERS[2]));
 	}
 	
 	@Test
 	void R31_canUse() {
-		fail();
+		assertTrue(DEFAULT_SHIELD.canBeUsedBy(DEFAULT_PLAYERS[3]));
 	}
 	
 	@Test
 	void R32_canUse() {
-		fail();
+		assertTrue(DEFAULT_SHIELD.canBeUsedBy(DEFAULT_PLAYERS[4]));
 	}
 	
 	@Test
 	void R33_canUse() {
-		fail();
+		assertTrue(DEFAULT_SHIELD.canBeUsedBy(DEFAULT_PLAYERS[5]));
 	}
 	
 	@Test
 	void R34_canUse() {
-		fail();
+		assertTrue(DEFAULT_SHIELD.canBeUsedBy(DEFAULT_PLAYERS[6]));
 	}
 	
 	@Test
 	void R35_canUse() {
-		fail();
+		assertTrue(DEFAULT_SHIELD.canBeUsedBy(DEFAULT_PLAYERS[7]));
 	}
 	
 	@Test
 	void R36_canUse() {
-		fail();
+		assertTrue(DEFAULT_SHIELD.canBeUsedBy(DEFAULT_PLAYERS[8]));
 	}
 	
 }
