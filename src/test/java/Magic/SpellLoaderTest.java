b3d177a8c2d8d6ed9c7bf0d59e6dfe7b0c4948f1
Olpan-cmd <63874865+Olpan-cmd@users.noreply.github.com>
2021-10-20 15:09:55 +0200

    SpellLoader: refactored tests into individual assertEqualsTests and added IllegalStateException with TTD

--- src/test/java/Magic/SpellLoaderTest.java
+++ src/test/java/Magic/SpellLoaderTest.java
@@ -3,12 +3,44 @@ package Magic;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class SpellLoaderTest {
 
+
+    FileWriter fr = new FileWriter("src/resources/SpellDataTest/FaultySpellDataTest");
+    PrintWriter pr = new PrintWriter(fr);
+
+    public SpellLoaderTest() throws IOException {
+    }
+
+    @Test
+    void wrongNumberOfArgumentsDDS() {
+        pr.write("spells:\n");
+        pr.write("DamageDealingSpell,Snowball,5,water");
+        pr.close();
+        assertThrows(IllegalStateException.class, () -> {
+           new SpellLoader().loadSpells("src/resources/SpellDataTest/FaultySpellDataTest");
+        });
+
+    }
+
+    @Test
+    void wrongNumberOfArgumentsHS() {
+        pr.write("spells:\n");
+        pr.write("HealingSpell,Snowball,5,water");
+        pr.close();
+        assertThrows(IllegalStateException.class, () -> {
+            new SpellLoader().loadSpells("src/resources/SpellDataTest/FaultySpellDataTest");
+        });
+
+    }
+
     //Tests the Loader from the txt-file SpellDataTest.txt which reads:
-    //
     //Spells:
     //DamageDealingSpell,Fireball,25,Fire,50
     //HealingSpell,Band-Aid,10,physical,15
@@ -16,40 +48,59 @@ public class SpellLoaderTest {
     ArrayList<Spell> testSpells = new SpellLoader().loadSpells("src/resources/SpellDataTest/SpellDataTest");
 
     @Test
-    void testLoadedDamageDealingSpell() {
-        Spell spell = testSpells.get(0);
-        assertTrue(spell instanceof DamageDealingSpell);
-        assertEquals("Fireball", spell.getName());
-        assertEquals(25, spell.getManaCost());
-        assertEquals(Element.FIRE, spell.getElement());
-        assertEquals(50, ((DamageDealingSpell) spell).getBaseDamage());
+    void correctlyLoadingDamageDealingSpellName() {
+        Spell spell = new DamageDealingSpell("Fireball", 25, Element.FIRE, 50);
+        Spell actualSpell = testSpells.get(0);
+        assertEquals(spell.getName(), actualSpell.getName());
     }
 
     @Test
-    void testLoadedHealingSpell() {
-        Spell spell = testSpells.get(1);
-        assertTrue(spell instanceof HealingSpell);
-        assertEquals("Band-Aid", spell.getName());
-        assertEquals(10, spell.getManaCost());
-        assertEquals(Element.PHYSICAL, spell.getElement());
-        assertEquals(15, ((HealingSpell) spell).getBaseHeal());
+    void correctlyLoadingDamageDealingSpellManaCost() {
+        Spell spell = new DamageDealingSpell("Fireball", 25, Element.FIRE, 50);
+        Spell actualSpell = testSpells.get(0);
+        assertEquals(spell.getManaCost(), actualSpell.getManaCost());
     }
 
     @Test
-    void testLoadedBuffSpell() {
-
+    void correctlyLoadingDamageDealingSpellElement() {
+        Spell spell = new DamageDealingSpell("Fireball", 25, Element.FIRE, 50);
+        Spell actualSpell = testSpells.get(0);
+        assertEquals(spell.getElement(), actualSpell.getElement());
     }
 
     @Test
-    void testLoadedDebuffSpell() {
-
+    void correctlyLoadingDamageDealingSpellBaseDamage() {
+        DamageDealingSpell spell = new DamageDealingSpell("Fireball", 25, Element.FIRE, 50);
+        Spell actualSpell = testSpells.get(0);
+        assertEquals(spell.getBaseDamage(), ((DamageDealingSpell)actualSpell).getBaseDamage());
     }
 
     @Test
-    void testMissingParameterInTxtFile() {
-        assertThrows(IndexOutOfBoundsException.class, () -> {
-            new SpellLoader().loadSpells("src/resources/SpellDataTest/FaultySpellDataTest1");
-        });
+    void correctlyLoadingHealingSpellName() {
+        HealingSpell spell = new HealingSpell("Band-Aid", 10, Element.PHYSICAL, 15);
+        Spell actualSpell = testSpells.get(1);
+        assertEquals(spell.getName(), actualSpell.getName());
+    }
+
+    @Test
+    void correctlyLoadingHealingSpellManaCost() {
+        HealingSpell spell = new HealingSpell("Band-Aid", 10, Element.PHYSICAL, 15);
+        Spell actualSpell = testSpells.get(1);
+        assertEquals(spell.getManaCost(), actualSpell.getManaCost());
+    }
+
+    @Test
+    void correctlyLoadingHealingSpellElement() {
+        HealingSpell spell = new HealingSpell("Band-Aid", 10, Element.PHYSICAL, 15);
+        Spell actualSpell = testSpells.get(1);
+        assertEquals(spell.getElement(), actualSpell.getElement());
+    }
+
+    @Test
+    void correctlyLoadingHealingSpellBaseHeal() {
+        HealingSpell spell = new HealingSpell("Band-Aid", 10, Element.PHYSICAL, 15);
+        Spell actualSpell = testSpells.get(1);
+        assertEquals(spell.getBaseHeal(), ((HealingSpell)actualSpell).getBaseHeal());
     }
 
 }
